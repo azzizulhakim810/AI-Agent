1891fbe50ecfcc2183f39ebad32ce1b79ce1d314
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-04 19:21:36 +0600

    Streaming response lets the response flow chunk by chunk but it doesn't work

--- backend/src/agent.ts
+++ backend/src/agent.ts
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
 import "dotenv/config";
 
 // const ai = new GoogleGenAI({});
@@ -9,14 +9,26 @@ import "dotenv/config";
 //   return `Agent: ${userMessage}`;
 // }
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY as string });
 // const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 // const model = genAI.getGenerativeModel({model: "gemini-3-pro-preview"})
 
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY as string });
+
 export async function runAgent(userMessage: string) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
+    model: "gemini-2.5-flash-lite",
     contents: `User says ${userMessage}. You're an Intelligent AI Agent. Reply relevant answer with the user in a friendly way. `,
   });
   return response.text;
 }
+// export async function runAgent(userMessage: string) {
+//   const response = await ai.models.generateContentStream({
+//     model: "gemini-2.5-flash-lite",
+//     contents: `User says ${userMessage}. You're an Intelligent AI Agent. Reply relevant answer with the user in a friendly way. `,
+//   });
+
+//   for await (const chunk of response) {
+//     return chunk;
+//     // console.log(chunk);
+//   }
+// }
